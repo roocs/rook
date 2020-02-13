9694fd13e7c6636075e7e099d660e639c599c7e3
Pingu Carsti <ehbrecht@dkrz.de>
2020-02-13 18:21:42 +0100

    added retrieve process

--- roocswps/processes/__init__.py
+++ roocswps/processes/__init__.py
@@ -1,9 +1,11 @@
 from .wps_say_hello import SayHello
 from .wps_subset import Subset
 from .wps_average import Average
+from .wps_retrieve import Retrieve
 
 processes = [
     SayHello(),
     Subset(),
     Average(),
+    Retrieve(),
 ]
