0fedc1716093da0b8faf0e55a410a37ab948f06e
Pingu Carsti <ehbrecht@dkrz.de>
2020-02-13 18:46:28 +0100

    added orchestrate process

--- roocswps/processes/__init__.py
+++ roocswps/processes/__init__.py
@@ -2,10 +2,12 @@ from .wps_say_hello import SayHello
 from .wps_subset import Subset
 from .wps_average import Average
 from .wps_retrieve import Retrieve
+from .wps_orchestrate import Orchestrate
 
 processes = [
     SayHello(),
     Subset(),
     Average(),
     Retrieve(),
+    Orchestrate(),
 ]
