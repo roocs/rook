4aebb8846aacb62efdf8d54d8643afee8c5c7b4c
Pingu Carsti <ehbrecht@dkrz.de>
2020-02-21 15:52:18 +0100

    added daops

--- roocswps/processes/__init__.py
+++ roocswps/processes/__init__.py
@@ -1,11 +1,11 @@
 from .wps_subset import Subset
 from .wps_average import Average
-from .wps_retrieve import Retrieve
+# from .wps_retrieve import Retrieve
 from .wps_orchestrate import Orchestrate
 
 processes = [
     Subset(),
     Average(),
-    Retrieve(),
+    # Retrieve(),
     Orchestrate(),
 ]
--- roocswps/processes/wps_subset.py
+++ roocswps/processes/wps_subset.py
@@ -1,25 +1,41 @@
 from pywps import Process, LiteralInput, ComplexOutput
 from pywps import FORMATS
+from pywps.app.exceptions import ProcessError
 
+import daops
+import daops.config as dconfig
 
 class Subset(Process):
     def __init__(self):
         inputs = [
-            LiteralInput('period', 'Time Period',
-                         abstract='Please enter the time period: 195001-190012',
+            LiteralInput('data_ref', 'Data references',
                          data_type='string',
-                         min_occurs=0,
-                         max_occurs=1,),
-            LiteralInput('area', 'Bounding Box',
-                         abstract='Please enter the bbox: -10, -10, 10, 10',
+                         default="cmip6.ukesm1.r1.gn.tasmax.v20200101",
+                         min_occurs=1,
+                         max_occurs=10,),
+            LiteralInput('time', 'Time Period',
                          data_type='string',
-                         min_occurs=0,
+                         default="1999-01-01T00:00:00/2100-12-30T00:00:00",
+                         min_occurs=1,
                          max_occurs=1,),
+            LiteralInput('space', 'Bounding Box',
+                         data_type='string',
+                         default='-5.,49.,10.,65',
+                         min_occurs=1,
+                         max_occurs=1,),
+            LiteralInput('level', 'Level',
+                         data_type='integer',
+                         default='1000',
+                         min_occurs=1,
+                         max_occurs=10,),
+            LiteralInput('pre_checked', 'Pre-Checked', data_type='boolean',
+                         abstract='Use checked data only.',
+                         default='0'),
         ]
         outputs = [
             ComplexOutput('output', 'Output',
                           as_reference=True,
-                          supported_formats=[FORMATS.TEXT]),
+                          supported_formats=[FORMATS.NETCDF]),
         ]
 
         super(Subset, self).__init__(
@@ -34,7 +50,20 @@ class Subset(Process):
             status_supported=True
         )
 
-    @staticmethod
-    def _handler(request, response):
-        response.outputs['output'].data = 'not working yet'
+    def _handler(self, request, response):
+        data_refs = [dref.data for dref in request.inputs['data_ref']]
+        if pre_checked and not daops.is_characterised(data_refs, require_all=True):
+            raise ProcessError('Data has not been pre-checked')
+
+        config_args = {'output_dir': self.workdir,
+                       'chunk_rules': dconfig.chunk_rules,
+                       'filenamer': dconfig.filenamer}
+
+        kwargs = {}
+        kwargs['time'] = request.inputs['time'][0].data
+        kwargs['space'] = request.inputs['space'][0].data
+        kwargs['level'] = request.inputs['level'][0].data
+        kwargs.update(config_args)
+
+        response.outputs['output'].file = daops.subset(data_refs, **kwargs)
         return response
