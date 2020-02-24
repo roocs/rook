105afe99bf9d8648d87062aff804c39dfc34e490
Pingu Carsti <ehbrecht@dkrz.de>
2020-02-24 19:53:09 +0100

    update subset

--- roocswps/processes/wps_subset.py
+++ roocswps/processes/wps_subset.py
@@ -10,27 +10,29 @@ class Subset(Process):
         inputs = [
             LiteralInput('data_ref', 'Data references',
                          data_type='string',
-                         default="cmip6.ukesm1.r1.gn.tasmax.v20200101",
+                         default='cmip5.output1.MOHC.HadGEM2-ES.rcp85.mon.atmos.Amon.r1i1p1.latest.tas',
                          min_occurs=1,
                          max_occurs=10,),
             LiteralInput('time', 'Time Period',
                          data_type='string',
-                         default="1999-01-01T00:00:00/2100-12-30T00:00:00",
+                         default="2085-01-01/2120-12-30",
                          min_occurs=1,
                          max_occurs=1,),
             LiteralInput('space', 'Bounding Box',
                          data_type='string',
                          default='-5.,49.,10.,65',
-                         min_occurs=1,
+                         min_occurs=0,
                          max_occurs=1,),
             LiteralInput('level', 'Level',
                          data_type='integer',
                          default='1000',
-                         min_occurs=1,
+                         min_occurs=0,
                          max_occurs=10,),
             LiteralInput('pre_checked', 'Pre-Checked', data_type='boolean',
                          abstract='Use checked data only.',
-                         default='0'),
+                         default='0',
+                         min_occurs=1,
+                         max_occurs=1),
         ]
         outputs = [
             ComplexOutput('output', 'Output',
@@ -52,18 +54,34 @@ class Subset(Process):
 
     def _handler(self, request, response):
         data_refs = [dref.data for dref in request.inputs['data_ref']]
-        if pre_checked and not daops.is_characterised(data_refs, require_all=True):
+        if request.inputs['pre_checked'][0].data and not daops.is_characterised(data_refs, require_all=True):
             raise ProcessError('Data has not been pre-checked')
 
-        config_args = {'output_dir': self.workdir,
-                       'chunk_rules': dconfig.chunk_rules,
-                       'filenamer': dconfig.filenamer}
+        if 'time' in request.inputs:
+            value = request.inputs['time'][0].data
+            time = value.split('/')
+        else:
+            time = None
 
-        kwargs = {}
-        kwargs['time'] = request.inputs['time'][0].data
-        kwargs['space'] = request.inputs['space'][0].data
-        kwargs['level'] = request.inputs['level'][0].data
-        kwargs.update(config_args)
+        if 'space' in request.inputs:
+            value = request.inputs['space'][0].data
+            space = [float(_) for _ in value.split(',')]
+        else:
+            space = None
 
-        response.outputs['output'].file = daops.subset(data_refs, **kwargs)
+        if 'level' in request.inputs:
+            level = request.inputs['level'][0].data
+        else:
+            level = None
+
+        result = daops.subset(
+            data_refs,
+            time=time,
+            # space=space,
+            # level=level,
+            output_dir=self.workdir,
+            # chunk_rules=None,
+            # filenamer=None,
+            )
+        response.outputs['output'].file = result.file_paths[0]
         return response
