6cae819d3f8fc5ccec88dc12408b7caf5dcab09c
Pingu Carsti <ehbrecht@dkrz.de>
2020-02-25 14:20:01 +0100

    update average

--- roocswps/processes/wps_average.py
+++ roocswps/processes/wps_average.py
@@ -1,10 +1,18 @@
 from pywps import Process, LiteralInput, ComplexOutput
 from pywps import FORMATS
+from pywps.app.exceptions import ProcessError
+
+import daops
 
 
 class Average(Process):
     def __init__(self):
         inputs = [
+            LiteralInput('data_ref', 'Data references',
+                         data_type='string',
+                         default='cmip5.output1.MOHC.HadGEM2-ES.rcp85.mon.atmos.Amon.r1i1p1.latest.tas',
+                         min_occurs=1,
+                         max_occurs=10,),
             LiteralInput('axes', 'Axes',
                          abstract='Please choose an axes for averaging.',
                          data_type='string',
@@ -12,6 +20,11 @@ class Average(Process):
                          max_occurs=1,
                          default='time',
                          allowed_values=['time', 'latitude', 'longitude']),
+            LiteralInput('pre_checked', 'Pre-Checked', data_type='boolean',
+                         abstract='Use checked data only.',
+                         default='0',
+                         min_occurs=1,
+                         max_occurs=1),
         ]
         outputs = [
             ComplexOutput('output', 'Output',
@@ -33,5 +46,8 @@ class Average(Process):
 
     @staticmethod
     def _handler(request, response):
+        data_refs = [dref.data for dref in request.inputs['data_ref']]
+        if request.inputs['pre_checked'][0].data and not daops.is_characterised(data_refs, require_all=True):
+            raise ProcessError('Data has not been pre-checked')
         response.outputs['output'].data = 'not working yet'
         return response
