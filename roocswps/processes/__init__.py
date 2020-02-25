89d65466cf07e0b830af189eda094304bc578477
Pingu Carsti <ehbrecht@dkrz.de>
2020-02-25 13:59:12 +0100

    removed retrieve

--- roocswps/processes/__init__.py
+++ roocswps/processes/__init__.py
@@ -1,11 +1,9 @@
 from .wps_subset import Subset
 from .wps_average import Average
-# from .wps_retrieve import Retrieve
 from .wps_orchestrate import Orchestrate
 
 processes = [
     Subset(),
     Average(),
-    # Retrieve(),
     Orchestrate(),
 ]
