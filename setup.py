e3f6f98b516d83803613a9be28d97fc2a0ea0444
MacPingu <cehbrecht@users.noreply.github.com>
2020-06-18 11:34:51 +0200

    cookiecutter update with cruft (#18)
    * cookiectter update with cruft
    
    * disabled osx build

--- setup.py
+++ setup.py
@@ -10,6 +10,7 @@ from setuptools import setup, find_packages
 here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README.rst')).read()
 CHANGES = open(os.path.join(here, 'CHANGES.rst')).read()
+REQUIRES_PYTHON = ">=3.5.0"
 
 about = {}
 with open(os.path.join(here, 'rook', '__version__.py'), 'r') as f:
@@ -38,12 +39,14 @@ setup(name='rook',
       version=about['__version__'],
       description="A WPS service for roocs.",
       long_description=README + '\n\n' + CHANGES,
+      long_description_content_type="text/x-rst",
       author=about['__author__'],
       author_email=about['__email__'],
       url='https://github.com/roocs/rook',
+      python_requires=REQUIRES_PYTHON,
       classifiers=classifiers,
       license="Apache Software License 2.0",
-      keywords='wps pywps birdhouse roocs rook',
+      keywords='wps pywps birdhouse rook',
       packages=find_packages(),
       include_package_data=True,
       install_requires=reqs,
