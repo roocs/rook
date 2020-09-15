1240737ad6135437c96dea812713631a754b42d6
MacPingu <cehbrecht@users.noreply.github.com>
2020-09-16 01:28:05 +0200

    using patched pywps with fix for ProcessError (#50)
    * using patched pywps with fix for ProcessError
    
    * use pywps 4.2 branch

--- rook/utils.py
+++
@@ -1,9 +0,0 @@
-import re
-
-
-def format_error_message(text):
-    text = f"{text}"
-    # TODO: error message is validated by pywps
-    text = ' '.join(re.findall(r"['\w', '.', ':', '!', '?', '=', ',', '-']+", text.strip()))
-    text = text[:144]
-    return text
