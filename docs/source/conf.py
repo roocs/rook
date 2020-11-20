f6d273d5256afa3767200a17964f92c36bb1e9fd
Carsten Ehbrecht <ehbrecht@dkrz.de>
2020-11-20 11:06:26 +0100

    fix bumpversion

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -90,7 +90,7 @@ author = "Carsten Ehbrecht"
 # the built documents.
 #
 # The short X.Y version.
-version = "0.1.0"
+# version = "0.1.0"
 # The full version, including alpha/beta/rc tags.
 release = "0.1.0"
 
