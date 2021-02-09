7781b4e1123efa59cc9241f90d38374709fdd58c
Eleanor Smith <esmith88@sci5.jasmin.ac.uk>
2021-02-09 15:54:11 +0000

    update alignment to use times of day instead of just the date

--- rook/director/alignment.py
+++ rook/director/alignment.py
@@ -1,7 +1,9 @@
 import os
+import xarray as xr
 
 import dateutil.parser as parser
 from roocs_utils.parameter import time_parameter
+from roocs_utils.utils.time_utils import to_isoformat
 
 
 class SubsetAlignmentChecker:
@@ -10,7 +12,7 @@ class SubsetAlignmentChecker:
         self.is_aligned = False
         self.aligned_files = []
 
-        self._deduce_alignment(inputs)  # needs inputs - where are they coming from
+        self._deduce_alignment(inputs)
 
     def _deduce_alignment(self, inputs):
         # At present, we reject alignment if any "area" or "level" subset is requested
@@ -31,23 +33,11 @@ class SubsetAlignmentChecker:
             self._check_time_alignment(start, end)
 
     def _get_file_times(self, fpath):
-        # Use file name to get start and end (instead of reading files)
-        start, end = os.path.basename(fpath).split(".")[-2].split("_")[-1].split("-")
-
-        # parser should parse YYYYMM and YYYYMMDDhhmm but doesn't
-        start, end = [
-            each[:6] + "01" + each[6:] if len(each) == 6 else each
-            for each in (start, end)
-        ]
-        start, end = [
-            each[:8] + "T" + each[8:] if len(each) > 8 else each
-            for each in (start, end)
-        ]
-        start, end = (
-            parser.isoparse(start).isoformat(),
-            parser.isoparse(end).isoformat(),
-        )
-
+        # get start and end times from the time dimension in the file
+        ds = xr.open_dataset(fpath, use_cftime=True)
+        start = to_isoformat(ds.time.values[0])
+        end = to_isoformat(ds.time.values[-1])
+        ds.close()
         return start, end
 
     def _check_time_alignment(self, start, end):
@@ -70,6 +60,7 @@ class SubsetAlignmentChecker:
 
         # First of all truncate requested range to actual range if it extends
         # beyond the actual range in the files
+
         start_in_files, _ = self._get_file_times(self.input_files[0])
         _, end_in_files = self._get_file_times(self.input_files[-1])
 
@@ -94,10 +85,9 @@ class SubsetAlignmentChecker:
             if fend == end:
                 matches += 1
 
-            if fstart >= start or fend <= end:
+            if fstart >= start or end <= fend:
                 self.aligned_files.append(fpath)
 
-        # If there were not
         if matches != 2:
             self.aligned_files.clear()
             return
