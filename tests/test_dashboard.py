9551ea175460038c8fb5faa0fd094cda3be30405
Carsten Ehbrecht <ehbrecht@dkrz.de>
2021-06-18 16:55:56 +0200

    update dashboard test

--- tests/test_dashboard.py
+++ tests/test_dashboard.py
@@ -1,10 +1,13 @@
+import pytest
+
 from rook.dashboard import Dashboard
 
 
+@pytest.mark.xfail(reason="remote test data might not be available")
 def test_dashboard(tmpdir):
     dash = Dashboard(output_dir=tmpdir)
     dash.load(
-        "http://rook3.cloud.dkrz.de:80/outputs/rook/9963ecd6-cde8-11eb-bb2b-fa163e466023/wps_requests.csv",
+        "http://rook4.cloud.dkrz.de:80/outputs/rook/9e3ae300-d044-11eb-8ce3-fa163e1098db/wps_requests.csv",
         filter="orchestrate",
     )
     print(dash.write())
