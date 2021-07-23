0a1bcac4ad245506cf92a8f221d790412d368cbb
Carsten Ehbrecht <ehbrecht@dkrz.de>
2021-07-23 16:41:26 +0200

    fix prov tests

--- tests/test_wps_subset.py
+++ tests/test_wps_subset.py
@@ -58,11 +58,10 @@ def test_wps_subset_cmip6_prov():
     assert_response_success(resp)
     doc = prov.read(get_output(resp.xml)["prov"][len("file://") :])
     assert (
-        'activity(subset, -, -, [time="1860-01-01/1900-12-30", area="1,1,300,89", apply_fixes="0" %% xsd:boolean])'
-        in doc.get_provn()
+        'roocs:time="1860-01-01/1900-12-30", roocs:area="1,1,300,89"' in doc.get_provn()
     )
     assert (
-        "wasDerivedFrom(rlds_Amon_IPSL-CM6A-LR_historical_r1i1p1f1_gr_18600116-19001216.nc, CMIP6.CMIP.IPSL.IPSL-CM6A-LR.historical.r1i1p1f1.Amon.rlds.gr.v20180803, subset, -, -)"  # noqa
+        "wasDerivedFrom(roocs:rlds_Amon_IPSL-CM6A-LR_historical_r1i1p1f1_gr_18600116-19001216.nc, roocs:CMIP6.CMIP.IPSL.IPSL-CM6A-LR.historical.r1i1p1f1.Amon.rlds.gr.v20180803"  # noqa
         in doc.get_provn()
     )
 
@@ -79,16 +78,13 @@ def test_wps_subset_cmip6_multiple_files_prov():
     assert_response_success(resp)
     doc = prov.read(get_output(resp.xml)["prov"][len("file://") :])
     print(doc.get_provn())
+    assert 'roocs:time="1850-01-01/2013-12-30"' in doc.get_provn()
     assert (
-        'activity(subset, -, -, [time="1850-01-01/2013-12-30", apply_fixes="0" %% xsd:boolean])'
+        "wasDerivedFrom(roocs:siconc_SImon_MPI-ESM1-2-HR_historical_r1i1p1f1_gn_18500116-18960316.nc, roocs:CMIP6.CMIP.MPI-M.MPI-ESM1-2-HR.historical.r1i1p1f1.SImon.siconc.gn.latest"  # noqa
         in doc.get_provn()
     )
     assert (
-        "wasDerivedFrom(siconc_SImon_MPI-ESM1-2-HR_historical_r1i1p1f1_gn_18500116-18960316.nc, CMIP6.CMIP.MPI-M.MPI-ESM1-2-HR.historical.r1i1p1f1.SImon.siconc.gn.latest, subset, -, -)"  # noqa
-        in doc.get_provn()
-    )
-    assert (
-        "wasDerivedFrom(siconc_SImon_MPI-ESM1-2-HR_historical_r1i1p1f1_gn_18960416-19420616.nc, CMIP6.CMIP.MPI-M.MPI-ESM1-2-HR.historical.r1i1p1f1.SImon.siconc.gn.latest, subset, -, -)"  # noqa
+        "wasDerivedFrom(roocs:siconc_SImon_MPI-ESM1-2-HR_historical_r1i1p1f1_gn_18960416-19420616.nc, roocs:CMIP6.CMIP.MPI-M.MPI-ESM1-2-HR.historical.r1i1p1f1.SImon.siconc.gn.latest"  # noqa
         in doc.get_provn()
     )
 
