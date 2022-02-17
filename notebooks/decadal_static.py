e9afe6aecd3722d2c64d8dccd63d7bea1ca9846a
Carsten Ehbrecht <ehbrecht@dkrz.de>
2022-02-17 21:26:22 +0100

    update static nb

--- notebooks/decadal_static.py
+++ notebooks/decadal_static.py
@@ -6,20 +6,12 @@ from memory_profiler import profile
 
 @profile
 def get_decdal_static(basedir, time=None):
-    def process_one_path(path):
-        # use a context manager, to ensure the file gets closed after use
-        with xr.open_dataset(path) as ds:
-            # select time
-            if time is not None:
-                ds = ds.isel(time=time)
-            # load all data from the transformed dataset, to ensure we can
-            # use it after closing each original file
-            ds.load()
-            return ds
-
     data_dir = Path(basedir)
     paths = list(data_dir.glob("**/*.nc"))
-    ds = process_one_path(paths[0])
-    # write output
-    ds.to_netcdf("out/static.nc")
-    return ds
+    # use a context manager, to ensure the file gets closed after use
+    with xr.open_dataset(paths[0]) as ds:
+        # select time
+        if time is not None:
+            ds = ds.isel(time=time)
+        # write output
+        ds.to_netcdf("out/static.nc")
