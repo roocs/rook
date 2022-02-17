e8a93df3616faf7c47861ce055776696b59d0009
Carsten Ehbrecht <ehbrecht@dkrz.de>
2022-02-17 23:48:15 +0100

    update nbs for day datasets

--- notebooks/decadal_dynamic_agg.py
+++ notebooks/decadal_dynamic_agg.py
@@ -14,6 +14,7 @@ def get_decdal_dynamic_agg(basedir, time=None):
         paths,
         concat_dim="realization",
         combine="nested",
+        # chunks=500,
         parallel=False,
         preprocess=None,
     ) as ds:
