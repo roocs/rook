03194ffe7c214af172afa900188987d4f54865b9
Carsten Ehbrecht <ehbrecht@dkrz.de>
2022-03-18 16:07:57 +0100

    using tmp_path

--- tests/test_workflow.py
+++ tests/test_workflow.py
@@ -12,9 +12,9 @@ TREE_WF_2 = resource_file("subset_wf_2.json")
 TREE_WF_5 = resource_file("subset_wf_5.json")
 
 
-def test_validate_tree_wf():
+def test_validate_tree_wf(tmp_path):
     wfdoc = workflow.load_wfdoc(TREE_WF)
-    wf = workflow.TreeWorkflow(output_dir=tempfile.mkdtemp())
+    wf = workflow.TreeWorkflow(output_dir=tmp_path.as_posix())
     assert wf.validate(wfdoc) is True
 
 
@@ -36,21 +36,21 @@ def test_build_tree():
     ]
 
 
-def test_run_tree_wf():
-    wf = workflow.WorkflowRunner(output_dir=tempfile.mkdtemp())
+def test_run_tree_wf(tmp_path):
+    wf = workflow.WorkflowRunner(output_dir=tmp_path.as_posix())
     output = wf.run(TREE_WF)
     assert "tas_mon_HadGEM2-ES_rcp85_r1i1p1_20850101-21200101_avg-year.nc" in output[0]
 
 
-def test_run_tree_wf_2():
-    wf = workflow.WorkflowRunner(output_dir=tempfile.mkdtemp())
+def test_run_tree_wf_2(tmp_path):
+    wf = workflow.WorkflowRunner(output_dir=tmp_path.as_posix())
     output = wf.run(TREE_WF_2)
     assert "tas_mon_HadGEM2-ES_rcp85_r1i1p1_20900101-21000101_avg-year.nc" in output[0]
 
 
-def test_run_wf_cmip6_subset_average():
+def test_run_wf_cmip6_subset_average(tmp_path):
     wfdoc = resource_file("wf_cmip6_subset_average.json")
-    wf = workflow.WorkflowRunner(output_dir=tempfile.mkdtemp())
+    wf = workflow.WorkflowRunner(output_dir=tmp_path.as_posix())
     output = wf.run(wfdoc)
     assert (
         "rlds_Amon_IPSL-CM6A-LR_historical_r1i1p1f1_gr_19850101-20140101_avg-year.nc"
@@ -58,8 +58,8 @@ def test_run_wf_cmip6_subset_average():
     )
 
 
-def test_run_tree_wf_5():
-    wf = workflow.WorkflowRunner(output_dir=tempfile.mkdtemp())
+def test_run_tree_wf_5(tmp_path):
+    wf = workflow.WorkflowRunner(output_dir=tmp_path.as_posix())
     output = wf.run(TREE_WF_5)
     assert (
         "rlds_Amon_IPSL-CM6A-LR_historical_r1i1p1f1_gr_19950101-20000101_avg-year.nc"
@@ -67,9 +67,9 @@ def test_run_tree_wf_5():
     )
 
 
-def test_wf_average_latlon_cmip6():
+def test_wf_average_latlon_cmip6(tmp_path):
     wfdoc = resource_file("wf_average_latlon_cmip6.json")
-    wf = workflow.WorkflowRunner(output_dir=tempfile.mkdtemp())
+    wf = workflow.WorkflowRunner(output_dir=tmp_path.as_posix())
     output = wf.run(wfdoc)
     # print(output)
     assert (
@@ -78,9 +78,9 @@ def test_wf_average_latlon_cmip6():
     )
 
 
-def test_wf_c3s_cmip6_collection_only():
+def test_wf_c3s_cmip6_collection_only(tmp_path):
     wfdoc = resource_file("wf_c3s_cmip6_subset_collection_only.json")
-    wf = workflow.WorkflowRunner(output_dir=tempfile.mkdtemp())
+    wf = workflow.WorkflowRunner(output_dir=tmp_path.as_posix())
     output = wf.run(wfdoc)
     expected_url = (
         "https://data.mips.copernicus-climate.eu/thredds/fileServer/esg_c3s-cmip6/"
@@ -90,9 +90,9 @@ def test_wf_c3s_cmip6_collection_only():
     assert output[0] == expected_url
 
 
-def test_wf_c3s_cmip6_original_files():
+def test_wf_c3s_cmip6_original_files(tmp_path):
     wfdoc = resource_file("wf_c3s_cmip6_subset_original_files.json")
-    wf = workflow.WorkflowRunner(output_dir=tempfile.mkdtemp())
+    wf = workflow.WorkflowRunner(output_dir=tmp_path.as_posix())
     output = wf.run(wfdoc)
     expected_url = (
         "https://data.mips.copernicus-climate.eu/thredds/fileServer/esg_c3s-cmip6/"
