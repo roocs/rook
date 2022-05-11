8071ecf9a145a067c975541801ae57c99a9e0429
Carsten Ehbrecht <ehbrecht@dkrz.de>
2022-05-11 21:33:27 +0200

    added orchestrate test for metadata

--- tests/test_wps_orchestrate.py
+++ tests/test_wps_orchestrate.py
@@ -1,9 +1,12 @@
 import pytest
 
+import xarray as xr
 import prov
 
 from pywps import Service
 from pywps.tests import assert_response_success, client_for
+
+from tests.smoke.utils import parse_metalink
 
 from rook.processes.wps_orchestrate import Orchestrate
 
@@ -105,3 +108,36 @@ def test_wps_orchestrate_average_latlon_cmip6():
     print(doc.get_provn())
     assert 'time="1985-01-01/2014-12-30"' in doc.get_provn()
     assert 'dims="latitude,longitude"' in doc.get_provn()
+
+
+def test_wps_orchestrate_c3s_cmip6_subset_metadata():
+    client = client_for(Service(processes=[Orchestrate()], cfgfiles=[PYWPS_CFG]))
+    datainputs = "workflow=@xlink:href=file://{}".format(
+        resource_file("wf_c3s_cmip6_subset.json")
+    )
+    resp = client.get(
+        "?service=WPS&request=Execute&version=1.0.0&identifier=orchestrate&datainputs={}".format(
+            datainputs
+        )
+    )
+    assert_response_success(resp)
+    m_path = get_output(resp.xml)["output"]
+    # print(m_path)
+    # parse metalink
+    xml = open(m_path[7:], "r").read()
+    urls = parse_metalink(xml)
+    # print(urls)
+    ds = xr.open_dataset(urls[0][7:], use_cftime=True)
+    # check fill value in bounds
+    assert "_FillValue" not in ds.lat_bnds.encoding
+    assert "_FillValue" not in ds.lon_bnds.encoding
+    assert "_FillValue" not in ds.time_bnds.encoding
+    # check fill value in coordinates
+    assert "_FillValue" not in ds.time.encoding
+    assert "_FillValue" not in ds.lat.encoding
+    assert "_FillValue" not in ds.lon.encoding
+    assert "_FillValue" not in ds.height.encoding
+    # check coordinates in bounds
+    assert "coordinates" not in ds.lat_bnds.encoding
+    assert "coordinates" not in ds.lon_bnds.encoding
+    assert "coordinates" not in ds.time_bnds.encoding
