433f2b7959c3847bc1987a4bb78e4475fec5dcaf
Carsten Ehbrecht <ehbrecht@dkrz.de>
2023-01-11 22:45:35 +0100

    fix concat op

--- rook/utils/concat_utils.py
+++ rook/utils/concat_utils.py
@@ -1,3 +1,73 @@
+from clisops.ops.average import average_over_dims as clisops_average_over_dims
+from roocs_utils.parameter import collection_parameter
+from roocs_utils.parameter import dimension_parameter
+
+from daops.ops.base import Operation
+from daops.processor import process
+from daops.utils import normalise
+
+
+class Concat(Operation):
+    def _resolve_params(self, collection, **params):
+        """
+        Resolve the input parameters to `self.params` and parameterise
+        collection parameter and set to `self.collection`.
+        """
+        dims = dimension_parameter.DimensionParameter(params.get("dims"))
+        collection = collection_parameter.CollectionParameter(collection)
+
+        self.collection = collection
+        self.params = {
+            "dims": dims,
+            "ignore_undetected_dims": params.get("ignore_undetected_dims"),
+        }
+
+    def get_operation_callable(self):
+        return clisops_average_over_dims
+
+    def calculate(self):
+        config = {
+            "output_type": self._output_type,
+            "output_dir": self._output_dir,
+            "split_method": self._split_method,
+            "file_namer": self._file_namer,
+        }
+
+        self.params.update(config)
+
+        # Normalise (i.e. "fix") data inputs based on "character"
+        norm_collection = normalise.normalise(self.collection, self._apply_fixes)
+
+        rs = normalise.ResultSet(vars())
+
+        # change name of data ref here
+        for dset, norm_collection in norm_collection.items():
+            # Process each input dataset (either in series or
+            # parallel)
+            print("dset", dset)
+            print("norm_collection", norm_collection)
+            rs.add(
+                dset,
+                process(self.get_operation_callable(), norm_collection, **self.params),
+            )
+
+        return rs
+
+
+def _concat(
+    collection,
+    dims=None,
+    ignore_undetected_dims=False,
+    output_dir=None,
+    output_type="netcdf",
+    split_method="time:auto",
+    file_namer="standard",
+    apply_fixes=True,
+):
+    result_set = Concat(**locals()).calculate()
+    return result_set
+
+
 def run_concat(args):
     result = concat(**args)
     return result.file_uris
@@ -12,7 +82,7 @@ def concat(
     apply_fixes=True,
 ):
     # dummy concat operator
-    from daops.ops.average import average_over_dims
+    # from daops.ops.average import average_over_dims
 
     args = dict(
         collection=collection,
@@ -23,4 +93,4 @@ def concat(
         file_namer=file_namer,
         apply_fixes=apply_fixes,
     )
-    return average_over_dims(**args)
+    return _concat(**args)
