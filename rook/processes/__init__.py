aa77642ce25b5fee8a28838203544b53c9677c1c
Pingu Carsti <ehbrecht@dkrz.de>
2023-09-25 17:23:02 +0200

    added operator weighted_average

--- rook/processes/__init__.py
+++ rook/processes/__init__.py
@@ -2,6 +2,7 @@ from .wps_usage import Usage
 from .wps_dashboard import DashboardProcess
 from .wps_average_time import AverageByTime
 from .wps_average_dim import AverageByDimension
+from .wps_average_weighted import WeightedAverage
 from .wps_orchestrate import Orchestrate
 from .wps_subset import Subset
 from .wps_concat import Concat
@@ -12,6 +13,7 @@ processes = [
     Subset(),
     AverageByTime(),
     AverageByDimension(),
+    WeightedAverage(),
     Concat(),
     Orchestrate(),
 ]
