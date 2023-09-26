86eb68bfbd72b2fbd8f44580909fe7711b31e7a7
Pingu Carsti <ehbrecht@dkrz.de>
2023-09-26 14:57:06 +0200

    fixed weighted average test

--- tests/test_wps_average_weighted.py
+++ tests/test_wps_average_weighted.py
@@ -13,7 +13,8 @@ def test_wps_average_weighted_cmip6():
     client = client_for(Service(processes=[WeightedAverage()], cfgfiles=[PYWPS_CFG]))
     datainputs = "collection=c3s-cmip6.ScenarioMIP.INM.INM-CM5-0.ssp245.r1i1p1f1.Amon.rlds.gr1.v20190619"
     resp = client.get(
-        f"?service=WPS&request=Execute&version=1.0.0&identifier=average&datainputs={datainputs}"
+        f"?service=WPS&request=Execute&version=1.0.0&identifier=weighted_average&datainputs={datainputs}"
     )
+    print(resp)
     assert_response_success(resp)
     assert "output" in get_output(resp.xml)
