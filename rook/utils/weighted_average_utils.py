e768713fca76dfc3e63e2ce665970c7b2464fcfa
Pingu Carsti <ehbrecht@dkrz.de>
2023-11-08 17:39:47 +0100

    updated w-avg

--- rook/utils/weighted_average_utils.py
+++ rook/utils/weighted_average_utils.py
@@ -1,16 +1,16 @@
 import numpy as np
+import xarray as xr
+from pathlib import Path
 
-import collections
+from typing import List, Optional, Sequence, Union
 
-from roocs_utils.project_utils import derive_ds_id
+from roocs_utils.parameter.dimension_parameter import DimensionParameter
 
-from daops.ops.base import Operation
-from daops.utils import normalise
-
-from clisops.ops import subset
+from daops.ops.average import Average as DaopsAverage
+from clisops.ops.average import Average as ClisopsAverage
 
 
-def apply_weighted_mean(ds):
+def calc_weighted_mean(ds):
     # fix cftime calendar
     ds["time"] = ds.indexes["time"].to_numpy()
     ds = ds.drop_vars(["time_bnds"])
@@ -25,46 +25,29 @@ def apply_weighted_mean(ds):
     return ds_weighted_mean
 
 
-class WeightedAverage(Operation):
+class WeightedAverage_(ClisopsAverage):
     def _calculate(self):
-        new_collection = collections.OrderedDict()
+        avg_ds = calc_weighted_mean(self.ds)
 
-        for dset in self.collection:
-            ds_id = derive_ds_id(dset)
-            new_collection[ds_id] = dset.file_paths
-
-        # Normalise data inputs
-        norm_collection = normalise.normalise(
-            new_collection, False  # self._apply_fixes
-        )
-
-        rs = normalise.ResultSet(vars())
-
-        # calculate weighted mean
-        datasets = []
-        for ds_id in norm_collection.keys():
-            ds = norm_collection[ds_id]
-            ds_mod = apply_weighted_mean(ds)
-            datasets.append(ds_mod)
-
-        processed_ds = datasets[0]
-
-        # subset
-        outputs = subset(
-            processed_ds,
-            time=None,
-            file_namer="simple",
-            output_type="netcdf",
-        )
-        # result
-        rs.add("output", outputs)
-
-        return rs
+        return avg_ds
 
 
-def run_weighted_average(args):
-    result = weighted_average(**args)
-    return result.file_uris
+def _weighted_average(
+    ds: Union[xr.Dataset, str],
+    dims: Optional[Union[Sequence[str], DimensionParameter]] = None,
+    ignore_undetected_dims: bool = False,
+    output_dir: Optional[Union[str, Path]] = None,
+    output_type: str = "netcdf",
+    split_method: str = "time:auto",
+    file_namer: str = "standard",
+) -> List[Union[xr.Dataset, str]]:
+    op = WeightedAverage_(**locals())
+    return op.process()
+
+
+class WeightedAverage(DaopsAverage):
+    def get_operation_callable(self):
+        return _weighted_average
 
 
 def weighted_average(
@@ -77,5 +60,10 @@ def weighted_average(
     file_namer="simple",
     apply_fixes=False,
 ):
-    result_set = WeightedAverage(**locals())._calculate()
+    result_set = WeightedAverage(**locals()).calculate()
     return result_set
+
+
+def run_weighted_average(args):
+    result = weighted_average(**args)
+    return result.file_uris
