0d3200e5b5c262721957ef55d70e5c4af24d375d
Carsten Ehbrecht <ehbrecht@dkrz.de>
2023-12-07 15:43:06 +0100

    added fix_time_components

--- rook/utils/input_utils.py
+++ rook/utils/input_utils.py
@@ -6,6 +6,26 @@ from roocs_utils.exceptions import InvalidProject
 from rook import CONFIG
 
 
+def fix_time_components(tc):
+    # Remove reduntant time-component parts to avoid for example issues with 360day calendars
+    tc_all_months = "month:jan,feb,mar,apr,may,jun,jul,aug,sep,oct,nov,dec"
+    tc_all_months_2 = "month:01,02,03,04,05,06,07,08,09,10,11,12"
+    tc_all_days = "day:01,02,03,04,05,06,07,08,09,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31"
+
+    tc_parts = tc.split("|")
+    new_tc_parts = []
+    for tc_part in tc_parts:
+        if tc_part == tc_all_months:
+            continue
+        if tc_part == tc_all_months_2:
+            continue
+        if tc_part == tc_all_days:
+            continue
+        new_tc_parts.append(tc_part)
+    new_tc = "|".join(new_tc_parts)
+    return new_tc
+
+
 def parse_wps_input(inputs, key, as_sequence=False, must_exist=False, default=None):
     if not inputs.get(key):
         if must_exist:
