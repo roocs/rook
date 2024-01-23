aa0e56bd1ed3651bc3b65c8f3ae3f5829f35caa0
Pingu Carsti <ehbrecht@dkrz.de>
2024-01-23 14:30:02 +0100

    fixed atlas_fix

--- rook/utils/apply_fixes.py
+++ rook/utils/apply_fixes.py
@@ -3,7 +3,7 @@ from .atlas_fixes import apply_atlas_fixes
 
 def apply_fixes(ds_id, ds):
     if ds_id.startswith("c3s-ipcc-atlas") or ds_id.startswith("c3s-cica-atlas"):
-        ds_mod = apply_atlas_fixes(ds_id, ds)
+        ds_mod = apply_atlas_fixes(ds)
     else:
         ds_mod = ds
     return ds_mod
--- rook/utils/atlas_fixes.py
+++ rook/utils/atlas_fixes.py
@@ -1,6 +1,20 @@
-def apply_atlas_fixes(ds_id, ds):
-    ds.sst.encoding["_FillValue"] = None
+import xarray as xr
+
+
+def apply_atlas_fixes(ds):
+    """
+    See also clisops.ops.base_operaton._remove_redundant_fill_values
+    """
+    if isinstance(ds, xr.Dataset):
+        var_list = list(ds.coords) + list(ds.data_vars)
+    elif isinstance(ds, xr.DataArray):
+        var_list = list(ds.coords)
+    for var in var_list:
+        ds[var].encoding["_FillValue"] = None
     for cvar in ["member_id", "gcm_variant", "gcm_model", "gcm_institution"]:
         for en in ["zlib", "shuffle", "complevel"]:
-            del ds[cvar].encoding[en]
+            try:
+                del ds[cvar].encoding[en]
+            except Exception:
+                pass
     return ds
