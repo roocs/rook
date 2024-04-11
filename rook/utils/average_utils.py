8a767e45f0f33e2da8941da263f8e22e8191ef3b
David Huard <huard.david@ouranos.ca>
2024-04-11 08:42:38 -0400

    removed artefact from merge

--- rook/utils/average_utils.py
+++ rook/utils/average_utils.py
@@ -18,13 +18,3 @@ def run_average_by_shape(args):
 
     result = average_shape(**args)
     return result.file_uris
-
-
-def run_weighted_average(args):
-    from daops.ops.average import average_over_dims
-
-    args["apply_fixes"] = False
-    args["dims"] = ["latitude", "longitude"]
-
-    result = average_over_dims(**args)
-    return result.file_uris
