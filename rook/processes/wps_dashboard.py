1e6f313898f0593bea69c57f8b6f984ac5709e13
MacPingu <cehbrecht@users.noreply.github.com>
2024-08-01 18:22:06 +0200

    Update dashboard (#255)
    * use async mode for dashboard
    
    * fix combine

--- rook/processes/wps_dashboard.py
+++ rook/processes/wps_dashboard.py
@@ -1,6 +1,6 @@
 import logging
 
-from pywps import FORMATS, ComplexOutput, Format, LiteralInput, Process
+from pywps import FORMATS, ComplexOutput, LiteralInput, Process
 from pywps.app.Common import Metadata
 from pywps.app.exceptions import ProcessError
 
@@ -23,13 +23,13 @@ class DashboardProcess(Process):
                 min_occurs=1,
                 max_occurs=1,
                 default="local",
-                allowed_values=["local", "ceda", "ipsl", "dkrz", "all"],
+                allowed_values=["local", "ipsl", "dkrz", "all"],
             ),
             LiteralInput(
                 "time",
                 "Time Period",
                 abstract="The time period for usage collection seperated by /"
-                "Example: 2021-04-01/2021-04-30",
+                "Example: 2024-06-01/2024-06-30",
                 data_type="string",
                 min_occurs=0,
                 max_occurs=1,
@@ -53,7 +53,7 @@ class DashboardProcess(Process):
             metadata=[
                 Metadata("ROOK", "https://github.com/roocs/rook"),
             ],
-            version="0.1",
+            version="0.2",
             inputs=inputs,
             outputs=outputs,
             store_supported=True,
@@ -69,7 +69,8 @@ class DashboardProcess(Process):
             time = None
             time_start = time_end = None
         try:
-            usage = Combine(site=request.inputs["site"][0].data)
+            site = request.inputs["site"][0].data
+            usage = Combine(site=site)
             fusage, fdownloads = usage.collect(
                 time_start=time_start, time_end=time_end, outdir=self.workdir
             )
--- rook/usage/combine.py
+++ rook/usage/combine.py
@@ -1,10 +1,11 @@
 import os
 import concurrent.futures
 import pandas as pd
+import time as time_
 
 from pywps import configuration as config
 
-from owslib.wps import WebProcessingService, SYNC
+from owslib.wps import WebProcessingService, ASYNC
 
 from .base import Usage
 
@@ -18,7 +19,19 @@ URLS = {
 
 def get_usage(site, time):
     wps = WebProcessingService(url=URLS[site])
-    resp = wps.execute(identifier="usage", inputs=[("time", time)], mode=SYNC)
+    resp = wps.execute(
+        identifier="usage",
+        inputs=[("time", time)],
+        mode=ASYNC,
+        output=[("wpsusage", True), ("downloads", True)],
+    )
+    while resp.isComplete() is False:
+        time_.sleep(10)
+        resp.checkStatus()
+
+    if not resp.isSucceded():
+        raise Exception("usage collection failed.")
+
     # requests
     df = pd.read_csv(
         resp.processOutputs[0].reference, parse_dates=["time_start", "time_end"]
@@ -48,7 +61,7 @@ class Combine(Usage):
     def __init__(self, site=None):
         site = site or "local"
         if site == "all":
-            self.sites = ["ceda", "dkrz"]
+            self.sites = ["ipsl", "dkrz"]
         else:
             self.sites = [site]
 
@@ -56,7 +69,7 @@ class Combine(Usage):
         time = format_time(time_start, time_end)
         df_list = []
         df_downloads_list = []
-        with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
+        with concurrent.futures.ThreadPoolExecutor(max_workers=2) as executor:
             jobs = {executor.submit(get_usage, site, time): site for site in self.sites}
             for future in concurrent.futures.as_completed(jobs):
                 site = jobs[future]
