0d46c42daa65b54deda217975161519d35d656a6
Carsten Ehbrecht <ehbrecht@dkrz.de>
2025-02-13 20:54:38 +0100

    using click.FileError in cli.py

--- src/rook/cli.py
+++ src/rook/cli.py
@@ -235,7 +235,7 @@ def start(
                 with PID_FILE.open("w") as fp:
                     fp.write(f"{pid}")
         except OSError as e:
-            raise Exception(f"{e.strerror} [{e.errno}]")
+            raise click.FileError(filename=f"{PID_FILE}", hint=f"{e.strerror} [{e.errno}]")
 
         if pid == 0:
             os.setsid()
