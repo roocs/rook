ae9cfa458b4dcb6ccfbede6b54ee036eaf854256
Pingu Carsti <ehbrecht@dkrz.de>
2026-07-02 15:58:37 +0200

    cleaned up helpers

--- src/rook/operations/processor.py
+++ src/rook/operations/processor.py
@@ -3,20 +3,8 @@
 from loguru import logger
 
 
-def dispatch(operation, dset, **kwargs):
-    """Dispatch the operation to the selected execution mode."""
-    logger.info("NOW SENDING TO PARALLEL DISPATCH MODE...")
-    return process(operation, dset, mode="serial", **kwargs)
-
-
-def process(operation, dset, mode="serial", **kwargs):
+def process(operation, dset, **kwargs):
     """Run processing operation on a dataset."""
     op_name = operation.__name__
-
-    if mode == "serial":
-        logger.info(f"Running {op_name} [{mode}]: on Dataset with args: {kwargs}")
-        result = operation(dset, **kwargs)
-    else:
-        result = dispatch(operation, dset, **kwargs)
-
-    return result
+    logger.info(f"Running {op_name}: on Dataset with args: {kwargs}")
+    return operation(dset, **kwargs)
